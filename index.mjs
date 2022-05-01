925ff9cc9e868c03773d11043bea7a9dfcd3faaf
Aleksey Timin <atimin@gmail.com>
2022-05-01 12:05:19 +0200

    fix 404 error

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import {Client, BucketSettings, QuotaType} from 'reduct-js';
+import {Client, QuotaType} from 'reduct-js';
 import crypto from 'crypto';
 import md5 from 'md5';
 
@@ -6,21 +6,21 @@ const serverUrl = process.env.REDUCT_STORAGE_URL;
 const size30Gb = 32212254720n;
 
 const client = new Client(serverUrl);
-const bigBlob = crypto.randomBytes(2 ** 20).toString('hex');
+const bigBlob = crypto.randomBytes(2 ** 20);
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 const writer = async (bucket) => {
   while (true) {
     const ts = BigInt(Date.now()) * 1000n;
-    const blob = bigBlob.substring(0,
+    const blob = bigBlob.slice(0,
         Math.round(Math.random() * (bigBlob.length - 1)));
 
     await bucket.write('blobs', blob, ts);
     await bucket.write('md-sums', md5(blob), ts);
 
-    console.log('Write blob with size %s kB',
-        Math.round(blob.length / 1024));
+    console.log('Write blob with size %s kB ts=%s',
+        Math.round(blob.length / 1024), ts);
     await sleep(100);
   }
 };
@@ -36,12 +36,18 @@ const reader = async (bucket) => {
         info.latestRecord);
     for (let i = 0; i < recordList.length; ++i) {
       console.log('Read record with ts=%s', recordList[i].timestamp);
-      const blob = await bucket.read('blobs', recordList[i].timestamp);
-      const mdSum = await bucket.read('md-sums', recordList[i].timestamp);
-
-      if (md5(blob) !== mdSum.toString()) {
-        throw Error("Wrong MD5 sum");
+      try {
+        const blob = await bucket.read('blobs', recordList[i].timestamp);
+        const mdSum = await bucket.read('md-sums', recordList[i].timestamp);
+        if (md5(blob) !== mdSum.toString()) {
+          throw Error('Wrong MD5 sum');
+        }
+      } catch (err) {
+        if (!err.code || err.code !== 404) {
+         throw err;
+        }
       }
+
     }
 
     await sleep(100);
